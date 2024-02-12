2ebe8ca772c7f7a9190d4912a571be4b7d3d7434
Bruce Wade <bruce.wade@gmail.com>
2024-02-11 21:13:29 -0800

    Implemented basic character movement
    Setup axis controller movements for forward/right, setup the ability to look around with mouse/keyboard/right thumbstick on controller. Finally added basic jump ability.

---
+++ Source/UE4Shooter/ShooterCharacter.h
@@ -0,0 +1,75 @@
+// Copyright Warply Designed
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class UE4SHOOTER_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AShooterCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called for forwards/backwards input
+	void MoveForward(float Value);
+
+	// Called for right/left input
+	void MoveRight(float Value);
+
+	 /**
+	  * Called via input to turn at a given rate.
+	  * @param Rate This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	  */
+	void TurnAtRate(float Rate);
+
+	/**
+	 * Called via input to look up/down at a given rate.
+	 * @param Rate This is a normaized rate, i.e. 1.0 means 100% of desired rate
+	 */
+	void LookUpAtRate(float Rate);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+
+	// Camera boom positioning the camera behind the character
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera", meta=(AllowPrivateAccess="true"))
+	USpringArmComponent* CameraBoom;
+
+	// Camera that follows the character
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera", meta=(AllowPrivateAccess="true"))
+	UCameraComponent* FollowCamera;
+
+	// Base turn rate, in deg/sec. Other scaling may affect final turn rate
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera", meta=(AllowPrivateAccess="true"))
+	float BaseTurnRate;
+
+	// Base look up/down rate in deg/sec. Other scaling may affect final turn rate
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera", meta=(AllowPrivateAccess="true"))
+	float BaseLookUpRate;
+	
+public:
+
+	// Returns CameraBoom subobject
+	FORCEINLINE USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+
+	// Returns FollowCamera subobject
+	FORCEINLINE UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+};
